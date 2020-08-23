9fe98f41e8f151e76be2d858cbc1c9c29cdc3f6a
Amey Chavan <chavanamey55@gmail.com>
2020-08-23 22:25:58 +0530

    Add files via upload

---
+++ y_fractal.py
@@ -0,0 +1,25 @@
+import turtle as t
+t.screensize(800,800)
+a = t.Turtle()
+
+a.left(90)
+a.penup()
+a.backward(100)
+a.pendown()
+a.speed(100)
+
+def y_fractal(length):		
+	if(length<10):	
+		return
+	else:
+		a.forward(length)
+		a.left(30)
+		y_fractal(3*(length/4))	
+		a.right(60)
+		y_fractal(3*length/4)
+		a.left(30)
+		a.backward(length)
+
+y_fractal(100)
+
+t.mainloop()
